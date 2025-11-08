6746c1a8eab5dee93a801736fd960f024a7cb323
doholee <doholee@outlook.com>
2025-11-08 22:18:03 +0900

    Add user registration

--- src/Section0_IntelliJ_Github_Sync.java
+++ src/Section0_IntelliJ_Github_Sync.java
@@ -2,5 +2,8 @@ public class Section0_IntelliJ_Github_Sync {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Sync Testing");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("인텔리제이 시팔");
     }
 }
